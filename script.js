79977c036150fef9f32e95bdae5672c32e43b034
Lylais <hollyleduc@protonmail.com>
2025-02-17 22:08:34 +0100

    ajout du code javascript

--- script.js
+++ script.js
@@ -1,75 +1,81 @@
+// variable
+
+let playerScore = 0;
+let computerScore = 0;
+let roundPlayed = 0;
+
+const winsAgainst = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
+};
+
+const roundPlayedArea = document.querySelector('.round-played');
+
+const playerScoreArea = document.querySelector('.player-score');
+const computerScoreArea = document.querySelector('.computer-score');
+const buttonRock = document.querySelector('.rock')
+const buttonPaper = document.querySelector('.paper');
+const buttonScissors = document.querySelector('.scissors');
+const roundResult = document.querySelector('.round-result');
+
+buttonRock.addEventListener('click', () => gameRound('rock'));
+buttonPaper.addEventListener('click', () => gameRound('paper'));
+buttonScissors.addEventListener('click', () => gameRound('scissors'));
+
+
 function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"]
     let randomComputerChoice = Math.floor(Math.random()*3);
-    if (randomComputerChoice === 0){
-        return "rock";
-    
-    } else if (randomComputerChoice === 1){
-        return "paper";
+    return choices[randomComputerChoice]
+}
+
+function gameRound(playerChoice){
+    let computerChoice = getComputerChoice();
+
+    if (playerChoice === computerChoice){
+        roundResult.textContent = "It's a tie!";
+        roundPlayed++;
+        roundPlayedArea.textContent = roundPlayed;
+    }
+
+    else if (winsAgainst[playerChoice] === computerChoice){
+        roundResult.textContent = `You win! ${playerChoice} beats ${computerChoice}`;
+        playerScore++
+        roundPlayed++;
+        roundPlayedArea.textContent = roundPlayed;
+        playerScoreArea.textContent = playerScore;
 
     } else {
-        return "scissors";
-    }
-}
-
-
-function getHumanChoice(){
-    let humanChoice;
-    const validChoice = ["rock", "paper", "scissors"];
-    while (!validChoice.includes(humanChoice)){
-        humanChoice = prompt("Please choose between rock, paper, and scissors : ");
-        humanChoice = humanChoice.toLowerCase();
-        if (!validChoice.includes(humanChoice)){
-            alert("Bad choice, please choose a valid option.")
-        }
-
+        roundResult.textContent = `You lose! ${computerChoice} beats ${playerChoice}`;
+        computerScore++;
+        roundPlayed++;
+        roundPlayedArea.textContent = roundPlayed;
+        computerScoreArea.textContent = computerScore;
     }
 
-    return humanChoice;
+    playGame()
 }
 
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+    roundPlayed = 0;
 
-function PlayGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+    playerScoreArea.textContent = playerScore;
+    computerScoreArea.textContent = computerScore;
+    roundPlayedArea.textContent = roundPlayed;
 
-    function playRound(humanChoice, computerChoice){
-        
-        if (humanChoice === computerChoice){
-            alert("It's a tie!");
+}
+function playGame(){
+    if (playerScore === 5){
+        roundResult.textContent = "You won the game!";
+        resetGame();
+    }
+    else if (computerScore === 5){
+        roundResult.textContent = "You lose the game!";
+        resetGame();
+    }
     
-        } else if (humanChoice === "rock" && computerChoice === "scissors"){
-            alert("You Won! Rock beats Scissors");
-            humanScore++ ;
-        } else if (humanChoice === "paper" && computerChoice === "rock"){
-            alert("You Won! Paper beats Rock");
-            humanScore++ ;
-        } else if (humanChoice === "scissors" && computerChoice === "paper"){
-            alert("You Won! Scissors beats Paper");
-            humanScore++;
-        } else {
-            alert(`You loose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-    
-    }
-
-    for (let i = 0; i < 5; i++) {
-        let computerChoice = getComputerChoice();
-        let humanChoice = getHumanChoice();
-        playRound(humanChoice, computerChoice);
-
-    }
-    if (humanScore > computerScore){
-        alert(`You Won the Game! | your score : ${humanScore} vs computerScore : ${computerScore}`);
-
-    } else if (humanScore < computerScore){
-        alert(`You Loose the Game! | your score : ${humanScore} vs computerScore : ${computerScore}`);
-        
-
-    } else {
-        alert(`It's a Draw! | your score : ${humanScore} vs computerScore : ${computerScore}`);
-
-    }
 }
 
-PlayGame();
